b91d66aa7366f40903502dba65763993aa7b2379
Brayton Stafford <brayton.stafford@teamofcare.com>
2019-12-31 15:04:21 -0600

    o Update named exports for PatientBanner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import PatientCard from './components/patient/PatientCard'
 import PatientTable from './components/patient/PatientTable'
 import PatientDetail from './components/patient/PatientDetail'
+import PatientBanner from './components/patient/PatientBanner'
 import ObservationTable from './components/observation/ObservationTable'
 import ObservationDetail from './components/observation/ObservationDetail'
 
@@ -8,6 +9,7 @@ export {
   PatientCard,
   PatientTable,
   PatientDetail,
+  PatientBanner,
   ObservationTable,
   ObservationDetail,
 }
